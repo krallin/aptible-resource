9f60124f76d8f32a9ca5843c1c8efe574603ab2b
Skylar Anderson <Skylar@Anderson.is>
2014-12-03 13:14:35 -0500

    incrementing the correct version :rage:

--- lib/aptible/resource/version.rb
+++ lib/aptible/resource/version.rb
@@ -1,5 +1,5 @@
 module Aptible
   module Resource
-    VERSION = '0.2.7'
+    VERSION = '0.2.8'
   end
 end
--- lib/hyper_resource/version.rb
+++ lib/hyper_resource/version.rb
@@ -1,4 +1,4 @@
 class HyperResource
-  VERSION = '0.2.8'
+  VERSION = '0.2.5'
   VERSION_DATE = '2014-04-02'
 end
